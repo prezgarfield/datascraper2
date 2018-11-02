0190d03caacec34d7b67d07473d8c08766be36dc
prezgarfield <rgarfield.qc@gmail.com>
2018-11-02 12:55:35 -0400

    Project initialize

---
+++ datascraper.py
@@ -0,0 +1,73 @@
+from bs4 import BeautifulSoup
+import requests
+from requests import get
+import csv
+from googlesearch import search
+import lxml
+
+def googletest():
+    query = "rob garfield"
+    url = "https://google.com/search?q=" + query
+    raw = get(url).text
+    soup = BeautifulSoup(raw, 'lxml')
+
+    for chunk in soup.find_all('span', class_='st'):
+        #chunk = soup.find_all('span', class_='st')  
+        print(chunk)
+        print()
+
+    #for j in search(query, tld="com", num=10, stop=1, pause=2):
+     #   print(j)
+
+def scrapecorey():
+    source = requests.get('http://cnn.com').text
+
+    soup = BeautifulSoup(source, 'lxml')
+
+    csv_file = open('cms_scrape.csv', 'w')
+
+    csv_writer = csv.writer(csv_file)
+    csv_writer.writerow(['headline', 'summary', 'video_link'])
+
+    for article in soup.find_all('article'):
+        headline = article.h2.a.text
+        print(headline)
+
+        summary = article.find('div', class_='entry-content').p.text
+        print(summary)
+
+        try:
+            vid_src = article.find('iframe', class_='youtube-player')['src']
+
+            vid_id = vid_src.split('/')[4]
+            vid_id = vid_id.split('?')[0]
+
+            yt_link = f'https://youtube.com/watch?v={vid_id}'
+        except Exception as e:
+            yt_link = None
+
+            print(yt_link)
+
+            print()
+
+            csv_writer.writerow([headline, summary, yt_link])
+
+        csv_file.close()
+
+def pullLinks(url):
+    source = requests.get(url).text
+    soup = BeautifulSoup(source, 'lxml')
+    body = soup.find('body')
+#    print(body)
+
+    for link in body.find_all('a'):
+        print(link.get('href'))
+
+def hello():
+    print("Working on it...")
+
+if __name__ == '__main__':
+    hello()
+ #   scrapecorey()
+    googletest()
+    #pullLinks("http://cnn.com")
